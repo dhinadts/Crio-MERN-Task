7fbfa9ea5aa1f76a3ef2bd84ce3ce7e7ee4ae1f7
dhinadts <dhinadts@gmail.com>
2026-01-30 20:17:54 +0530

    Backend UL eplaced

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8082/tasks";
+const API_URL = "https://crio-mern-task.onrender.com/tasks";
 
 export const fetchTasks = async () => {
     const response = await axios.get(API_URL);
